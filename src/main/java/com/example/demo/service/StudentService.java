224ccbc05d2c656e1b234fce7e3e23ac0c46d2cd
Kaixin Huang <huang_kaixin2001@163.com>
2024-10-16 15:09:15 -0400

    first commnit

---
+++ src/main/java/com/example/demo/service/StudentService.java
@@ -0,0 +1,19 @@
+package com.example.demo.service;
+
+import com.example.demo.dao.Student;
+import com.example.demo.dto.StudentDTO;
+
+public interface StudentService {
+
+     StudentDTO getStudentById(long id);
+
+     Long addNewStudent(StudentDTO studentDTO);
+
+     void deleteStudentById(long id);
+
+     StudentDTO updateStudentById(long id, String name, String email);
+
+//     Student save(Student student);
+//
+//     Student getStudent(Student student);
+}
